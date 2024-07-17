f8013ab5b26be8b2b11e666cff1babd282dfe39b
Midomik <krastikrabbs9234@gmail.com>
2024-07-18 00:12:37 +0300

    feat: create repo

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,7 +2,7 @@ export const Home = () => {
   return (
     <>
       <div className=" flex items-center justify-center text-[30px] text-[blue]">
-        Welcome to template!
+        Welcome to LearnLingo!
       </div>
     </>
   );

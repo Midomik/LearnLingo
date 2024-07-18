9d72bf8a7ac12b88cecb573c633e30d1a163889a
Midomik <krastikrabbs9234@gmail.com>
2024-07-19 00:27:44 +0300

    feat: setting routing

--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import App from './App';
-import { Home, NotFound } from '../pages';
+import { Home, NotFound, Teachers } from '../pages';
 // import { PrivateRoute } from '../app/providers/PrivateRoute';
 // import { PublicRoute } from '../app/providers/PublicRoute';
 
@@ -9,6 +9,8 @@ export const Router = () => {
     <Routes>
       <Route path="/" element={<App />}>
         <Route index element={<Home />} />
+        <Route path="teachers" element={<Teachers />} />
+
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
---
+++ src/pages/Teachers/index.jsx
@@ -0,0 +1,9 @@
+export const Teachers = () => {
+  return (
+    <>
+      <div className=" flex items-center justify-center text-[30px] text-[blue]">
+        Teachers page
+      </div>
+    </>
+  );
+};
